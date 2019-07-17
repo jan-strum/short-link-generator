2b248c1e164ba7e0d3b364c8e9ea8212d2ccd057
Jan-Strum <janusbased@gmail.com>
2019-07-17 15:17:04 -0500

    Copy to clipboard dynamic button text fully functional.

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default class CopyToClipboard extends React.Component {
   render() {
     const { copyToClipboard, shortLink, currentClipboard } = this.props
-    console.log(shortLink)
 
     return (
       <button onClick={() => copyToClipboard(shortLink)}>
